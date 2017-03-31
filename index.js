0a9e9f11c1a404ed857c5be5af837caba0d235a5
Captain Fantasy <captain@fantasyui.com>
2017-03-31 18:57:01 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+class OnlineMarketplace {
+  constructor(configuration) {
+    let defaults = {};
+    this.options = Object.assign({}, defaults, configuration);
+   }
+}
+module.exports = OnlineMarketplace;
