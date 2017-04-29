f9ad4cb92b5753352f59870d81927f0a3437c159
Captain Fantasy <captain@fantasyui.com>
2017-04-29 14:02:28 -0400

    Increase file download security

--- system/handler/download-file/index.js
+++ system/handler/download-file/index.js
@@ -3,44 +3,47 @@ const downloadKeySecret = process.env.DOWNLOAD_SECRET_KEY||'sk_test_c40aeeb53578
 const fs = require("fs");
 const path = require("path");
 const mime = require("mime");
-
+const url = require('url');
 const crypto = require("crypto");
-
 const kebabCase = require('lodash/kebabCase');
+const SecureLink = require("../../helpers/secure-link");
 
 module.exports = async function({route}){
 
   return async (req, res) => {
 
-    let customerEmail = req.query.email;
-    let productIdentity = req.query.product;
-    let productLicense = req.query.license;
-    let externalHash = req.query.serial;
+    try {
 
-    let shasum = crypto.createHash('sha1');
-    shasum.update([ customerEmail, productIdentity, productLicense, downloadKeySecret].join("::"));
-    let internalHash = shasum.digest('hex');
+      let secureLink2 = new SecureLink();
+      const myURL = url.parse( req.originalUrl )
+      secureLink2.decode( myURL.query ); // will throw on errors
+      const productIdentity = secureLink2.params().get("product");
 
-    if(internalHash === externalHash){
-
-      // HASH/LINK IS VALID.
       try {
+
         let file = `files/${kebabCase(productIdentity)}.zip`;
         let filename = path.basename(file);
         let mimetype = mime.lookup(file);
+
         res.setHeader('Content-disposition', 'attachment; filename=' + filename);
         res.setHeader('Content-type', mimetype);
         let filestream = fs.createReadStream(file);
         filestream.pipe(res);
+
       }catch(e){
+
         console.log(e);
+        res.render("error", {message: `Failure to stream file ${kebabCase(productIdentity)}`} );
+        res.status(500);
+
       }
 
-    }else{
-      res.render("error", {message: `Invalid Serial Number`} );
-      res.status(500);
-    }
+    }catch(e){
 
+      res.render("error", {message: e.message} );
+      res.status(500);
+
+    }
 
   }
 
