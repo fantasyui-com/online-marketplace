f54019ae888d852a4c858570b0e9317659abf45c
Captain Fantasy <captain@fantasyui.com>
2017-05-12 16:16:21 -0400

    Add Code Documentation

--- system/helpers/product-expander/index.js
+++ system/helpers/product-expander/index.js
@@ -1,6 +1,15 @@
 module.exports = function(i){
 
+  /*
+
+    Here we deal with expanding base information so that views can just print values without any magic.
+    1. All prices are in cents under amount property, this needs to be expanded.
+    2. A product link should be defined here (specifically for online-marketplace purposes)
+
+  */
+
     i.amount = i.licensing[0].amount; // copy the first which is the standard price.
+
     i.price = (parseInt(i.licensing[0].amount)/100).toFixed(2);// + " " + i.currency.toUpperCase();
     i.price = i.price.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 
--- system/value/browse-products/index.js
+++ system/value/browse-products/index.js
@@ -1,6 +1,16 @@
 const db = require("product-catalog");
 const expander = require("../../helpers/product-expander");
 
+/*
+
+  Data exposed through here is accessed by a view as follows:
+
+  {{#each browseProducts}}
+      {{> productCard}}
+  {{~/each}}
+
+*/
+
 module.exports = function({model,variable}){
 
   let result = db().filter(['deleted', false]).result();
