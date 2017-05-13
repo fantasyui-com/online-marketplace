d465581977cb5743ab9ef25ea905e42db783a8bd
Captain Fantasy <captain@fantasyui.com>
2017-05-13 17:57:01 -0400

    Hardcode devel server settings

--- server.devel.js
+++ server.devel.js
@@ -3,7 +3,7 @@ const system = require(path.join(__dirname, 'index.js'));
 const pkg = require(path.join(__dirname, 'package.json'));
 async function main() {
   let app = await system.install(pkg);
-  const listener = app.listen(pkg.config.port, pkg.config.host, () => {
+  const listener = app.listen('8080', '0.0.0.0', () => {
     console.log(`http://${listener.address().address}:${listener.address().port}/`);
     if (process.send) process.send('ready');
   });
