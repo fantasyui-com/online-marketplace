e783221d03c07bb50685dbc25164df50c5bcc58f
Captain Fantasy <captain@fantasyui.com>
2017-05-13 12:31:20 -0400

    Add GOOGLE_ANALYTICS_ID to data model

--- system/middleware/create-data-model/index.js
+++ system/middleware/create-data-model/index.js
@@ -6,16 +6,19 @@ module.exports = function({routes}){
   return function(req, res, next){
 
     const base = {
+
       program: pkg.name,
       author:  pkg.author,
       version: pkg.version,
       license: pkg.license,
+
     }
 
     req.model = Object.assign( {}, base, model);
 
     // Assign additional data to model.
     req.model.stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY || 'pk_test_6pRNASCoBOKtIshFeQd4XMUh';
+    req.model.googleAnalyticsID = process.env.GOOGLE_ANALYTICS_ID || 'UA-XXXXX-X';
 
     next();
 
