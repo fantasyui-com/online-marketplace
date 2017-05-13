e2c28b87bf21d36fee6b47a377deaf4285d3dee7
Captain Fantasy <captain@fantasyui.com>
2017-05-13 17:55:30 -0400

    Add missing param information via console.error

--- system/value/selected-product/index.js
+++ system/value/selected-product/index.js
@@ -5,7 +5,10 @@ module.exports = function({req, model, variable }){
 
   let result = db().filter(['deleted', false]).find({authorId:req.params.authorId, name:req.params.productName}).result();
 
-  if(!result) throw new Error('Product not found');
+  if(!result) {
+    throw new Error(`Product not found`);
+    console.error(`Product "${JSON.stringify({authorId:req.params.authorId, name:req.params.productName})}" not found`);
+  }
 
   result = expander(result);
 
