e634eb8684e9905295ebaf303fadb8ed5c141982
Captain Fantasy <captain@fantasyui.com>
2017-05-13 17:59:04 -0400

    Make production server default

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const system = require(path.join(__dirname, 'index.js'));
 const pkg = require(path.join(__dirname, 'package.json'));
-
+const createServer = require("auto-sni");
 async function main() {
-
-  let app = await system.install(pkg);
-
-  const listener = app.listen(pkg.config.port, pkg.config.host, () => {
-    console.log(`http://${listener.address().address}:${listener.address().port}/`);
+  const app = await system.install(pkg);
+  const conf = {
+    agreeTos: true, // Required for letsencrypt.
+    email: pkg.config.email,
+    domains: pkg.config.domains,
+    ports: pkg.config.ports
+  }
+  const server = createServer(conf, app);
+  server.once("listening", ()=> {
     if (process.send) process.send('ready');
   });
-
 }
-
 main();
