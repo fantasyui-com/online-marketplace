d7db61d3859681ede81cc7752884d65fdf250132
Captain Fantasy <captain@fantasyui.com>
2017-05-13 17:47:03 -0400

    Server for developers/development

---
+++ server.devel.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const system = require(path.join(__dirname, 'index.js'));
+const pkg = require(path.join(__dirname, 'package.json'));
+async function main() {
+  let app = await system.install(pkg);
+  const listener = app.listen(pkg.config.port, pkg.config.host, () => {
+    console.log(`http://${listener.address().address}:${listener.address().port}/`);
+    if (process.send) process.send('ready');
+  });
+}
+main();
