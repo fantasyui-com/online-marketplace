6a6ff8d6910fc12bdf45980def5b8fbceab29d66
Captain Fantasy <captain@fantasyui.com>
2017-05-13 19:23:05 -0400

    Secure error reporting (via uuid)

--- system/middleware/error-handler/index.js
+++ system/middleware/error-handler/index.js
@@ -1,15 +1,19 @@
 const path = require('path');
+const uuidV4 = require('uuid/v4');
+const localPath = path.resolve('./');
 
 module.exports = async function({app,conf}) {
   app.use(function (err, req, res, next) {
-    console.log('Error:', err)
-    let message = err.message|| err ||"Server Request Error";
-    let localPath = path.resolve('./');
-    // NETSEC: Sensitive Data Exposure, Local Path Exposure
-    message = message.replace(new RegExp(localPath,'g'),'...');
-    // NETSEC: Sensitive Data Exposure, Templating Engine Exposure
-    message = message.replace(new RegExp('.hbs','g'),'');
-    res.render("error", Object.assign({}, req.state, {message} ));
+
+    const uuid = uuidV4();
+
+    let message =  uuid + ": " + (err.message||err||"Server Request Error");
+    message = message.replace(new RegExp(localPath,'g'),'...'); // NETSEC: Sensitive Data Exposure, Local Path Exposure
+    message = message.replace(new RegExp('.hbs','g'),'xxx'); // NETSEC: Sensitive Data Exposure, Templating Engine Exposure
+
+    console.log('Error: ', message);
+    res.render("error", Object.assign({}, req.state, {message:'Error: '+uuid} ));
+
     res.status(500);
   });
 }
